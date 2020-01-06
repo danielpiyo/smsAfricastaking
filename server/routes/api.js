018aec5f88aa3dda630f87166381efb8b59b894b
danielpiyo <danielopiyo8@gmail.com>
2020-01-06 17:11:17 +0300

    first commit for sms challenge

---
+++ server/routes/api.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+// Use body parser to parse JSON body
+router.use(bodyParser.json());
+
+const options = {
+    apiKey: '6fe96d7615619f614f210ff7f300c43a70357fff90eab9683e29875b916f9979',         // use your sandbox app API key for development in the test environment
+    username: 'sandbox',      // use 'sandbox' for development in the test environment
+};
+
+const africasTalking = require('africastalking')(options);
+// Initialize a service e.g. SMS
+sms = africasTalking.SMS
+
+// Use the service
+router.post('/send', function(req, res){
+    var dataTosend = {
+        to: req.body.to,
+        message: req.body.message
+    }
+    
+    // Send message and capture the response or error
+    sms.send(dataTosend)
+        .then( response => {
+            console.log(response);
+            return res.contentType('application/json').status(201).send(JSON.stringify(response));
+        })
+        .catch( error => {
+            console.log(error);
+        });
+});
+
+module.exports = router;
